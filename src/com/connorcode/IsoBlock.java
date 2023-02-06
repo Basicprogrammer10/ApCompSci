f4b811f38b7f40588d1a876c16610f56531f5784
Connor Slade <connor@connorcode.com>
2023-02-06 17:39:13 -0500

    Load lib

---
+++ src/com/connorcode/IsoBlock.java
@@ -0,0 +1,11 @@
+package com.connorcode;
+
+import paintingcanvas.Canvas;
+
+public class IsoBlock {
+    static Canvas canvas = new Canvas(700, 500, "IsoBlock - Connor Slade");
+
+    public static void main(String[] args) {
+
+    }
+}
