909b26a5430618e52e3ec222dfc5e565b14a9110
Connor Slade <connor@connorcode.com>
2023-02-06 18:34:46 -0500

    Cleanup Loader

--- src/com/connorcode/IsoBlock.java
+++ src/com/connorcode/IsoBlock.java
@@ -1,40 +1,29 @@
 package com.connorcode;
 
 import paintingcanvas.Canvas;
-import paintingcanvas.drawable.Rectangle;
 import paintingcanvas.drawable.Square;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class IsoBlock {
     static final int PIXEL_SCALE = 8;
-    static final String[] BLOCKS = {
-            "acacia_log.png",
-            "birch_log.png",
-            "dark_oak_log.png",
-            "jungle_log.png",
-            "mangrove_log.png",
-            "oak_log.png",
-            "spruce_log.png",
-            "stripped_acacia_log.png",
-            "stripped_birch_log.png",
-            "stripped_dark_oak_log.png",
-            "stripped_jungle_log.png",
-            "stripped_mangrove_log.png",
-            "stripped_oak_log.png",
-            "stripped_spruce_log.png",
-            };
+    static final ClassLoader loader = IsoBlock.class.getClassLoader();
+    static final List<String> BLOCKS = new BufferedReader(new InputStreamReader(
+            Objects.requireNonNull(loader.getResourceAsStream("resources/IsoBlock/textures.txt")))).lines()
+            .filter(x -> !x.startsWith("#")).collect(Collectors.toUnmodifiableList());
     static Canvas canvas = new Canvas(700, 500, "IsoBlock - Connor Slade");
 
     public static void main(String[] args) {
-        var classLoader = IsoBlock.class.getClassLoader();
-        var textures = Arrays.stream(BLOCKS)
-                .map(i -> new Texture(classLoader.getResource("resources/IsoBlock/" + i)))
+        var textures = BLOCKS.stream()
+                .map(i -> new Texture(loader.getResource("resources/IsoBlock/" + i)))
                 .collect(Collectors.toList());
 
         for (int x = 0; x < 4; x++)
@@ -46,7 +35,7 @@ public class IsoBlock {
     }
 
     static class Block {
-        Rectangle[] elements;
+        Square[] elements;
         Texture texture;
 
         Block(Texture texture) {
@@ -54,9 +43,10 @@ public class IsoBlock {
         }
 
         void draw2D(int x, int y) {
+            elements = new Square[16 * 16];
             for (var xi = 0; xi < 16; xi++)
                 for (var yi = 0; yi < 16; yi++)
-                    new Square(x + xi * PIXEL_SCALE, y + yi * PIXEL_SCALE, PIXEL_SCALE).setColor(texture.get(xi, yi));
+                    elements[yi * 16 + xi] = new Square(x + xi * PIXEL_SCALE, y + yi * PIXEL_SCALE, PIXEL_SCALE).setColor(texture.get(xi, yi));
         }
     }
 
@@ -65,7 +55,7 @@ public class IsoBlock {
 
         // Load texture from file
         Texture(URL file) {
-            BufferedImage image = null;
+            BufferedImage image;
             try {
                 image = ImageIO.read(file);
             } catch (IOException e) {
